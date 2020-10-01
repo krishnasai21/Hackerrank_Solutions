3010946c1846e243d339cec8b8991e8e023f58ec
Krishna Sai Pala <34124167+krishnasai21@users.noreply.github.com>
2020-10-01 13:03:14 +0530

    first commit

---
+++ ArrayMaxsum.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+v = [0] * n
+
+for i in range(n):
+    if i < 2:
+        v[i] = max(arr[i], 0)
+        continue
+    elif i == 2:
+        v[i] = v[i - 2] + max(arr[i], 0)
+        continue
+
+    v[i] = max(v[i-2], v[i-3]) + max(arr[i], 0)
+
+print(max(v[-1], v[-2]))
