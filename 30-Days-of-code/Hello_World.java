de51d5a6f76948628a5e2c9cd76c34e2f3262f9d
Krishna Pala <palakrishnasai@gmail.com>
2021-01-26 10:54:45 +0530

    To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.  You've got this!
    Welcome to 30 Days of Code!
    Sample Output:
    
    Hello, World. 
    Welcome to 30 Days of Code!

---
+++ 30-Days-of-code/Hello_World.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        
+        String inputString = scan.nextLine();
+
+        
+        scan.close();
+
+        
+        System.out.println("Hello, World.");
+
+        
+        System.out.println(inputString);
+    }
+}
