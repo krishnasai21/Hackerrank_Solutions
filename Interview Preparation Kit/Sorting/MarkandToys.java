fb307791d43e337e82d3eb38f25bfbe4e39e78a1
Krishna Pala <palakrishnasai@gmail.com>
2021-05-17 10:54:37 +0530

    Create MarkandToys.java
    Sample Input
    
    7 50
    1 12 5 111 200 1000 10
    
    Sample Output
    
    4

---
+++ Interview Preparation Kit/Sorting/MarkandToys.java
@@ -0,0 +1,29 @@
+/*
+* @ author krishnasai21
+*
+*/
+import java.io.*;
+import java.util.*;
+
+public class MarkandToys {
+
+    public static void main(String[] args) throws IOException {
+       
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int k=Integer.parseInt(st.nextToken());
+        int[] ar=new int[n];
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            ar[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(ar);
+        int sum=0,count=0;
+        while(sum<=k){
+            sum=sum+ar[count];
+            count++;
+        }
+        System.out.println(count-1);
+    }
+}
