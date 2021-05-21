1cb5c1eb28137f5867d60e8e36a224e672e979b4
Krishna Pala <palakrishnasai@gmail.com>
2021-05-21 11:42:36 +0530

    Create Day5_PoissonDistribution_II.java
    Input Format
    
    A single line comprised of  "2" space-separated values denoting the respective means for  A and B  :
    
    0.88 1.55
    
    If you do not wish to read this information from stdin, you can hard-code it into your program.

---
+++ 10-Days-of-Statistics/Day5_PoissonDistribution_II.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+     
+        Scanner scan = new Scanner(System.in);
+        double A = scan.nextDouble();
+        double B = scan.nextDouble();
+        scan.close();
+        
+        
+        double dailyCostA = 160 + 40 * (A + (A * A));
+        double dailyCostB = 128 + 40 * (B + (B * B));
+
+        
+        System.out.format("%.3f%n", dailyCostA);
+        System.out.format("%.3f%n", dailyCostB);
+    }
+}
