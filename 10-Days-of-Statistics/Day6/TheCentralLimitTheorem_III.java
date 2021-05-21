cd764061a8b5f1441cac7521f8fca812fc322ec6
Krishna Pala <palakrishnasai@gmail.com>
2021-05-21 12:00:36 +0530

    Create TheCentralLimitTheorem_III.java
    There are five lines of input (shown below):
    
    100
    500
    80
    .95
    1.96
    The first line contains the sample size. The second and third lines contain the respective mean () and standard deviation (). The fourth line contains the distribution percentage we want to cover (as a decimal), and the fifth line contains the value of .
    
    If you do not wish to read this information from stdin, you can hard-code it into your program.

---
+++ 10-Days-of-Statistics/Day6/TheCentralLimitTheorem_III.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) {
+      
+        double mean   = 500;
+        double std    = 80;
+        int    n      = 100;
+        double zScore = 1.96; 
+        double marginOfError = zScore * std / Math.sqrt(n);
+
+        System.out.format("%.2f%n", mean - marginOfError);
+        System.out.format("%.2f%n", mean + marginOfError);
+    }
+}
