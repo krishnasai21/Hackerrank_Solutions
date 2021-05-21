d7c21bbb70fbef4043fbe065de3adcf103e16e7a
Krishna Pala <palakrishnasai@gmail.com>
2021-05-21 11:39:58 +0530

    Update PoissonDistribution_I.java

--- 10-Days-of-Statistics/Day5/PoissonDistribution_I.java
+++ 10-Days-of-Statistics/Day5/PoissonDistribution_I.java
@@ -1,31 +1,30 @@
+
+import java.util.Scanner;
+
 public class Solution {
 
     public static void main(String[] args) {
-        double mean = 20;
-        double std  = 2;
-        System.out.format("%.3f%n", cumulative(mean, std, 19.5));
-        System.out.format("%.3f%n", cumulative(mean, std, 22) - cumulative(mean, std, 20));
-    }
-
-    public static double cumulative(double mean, double std, double x) {
-        double parameter = (x - mean) / (std * Math.sqrt(2));
-        return (0.5) * (1 + erf(parameter));
-    }
+        /* Read and save input */
+        Scanner scan = new Scanner(System.in);
+        double lambda = scan.nextDouble();
+        int k = scan.nextInt();
+        scan.close();
         
-    public static double erf(double z) {
-        double t = 1.0 / (1.0 + 0.5 * Math.abs(z));
-
-        double ans = 1 - t * Math.exp( -z*z   -   1.26551223 +
-                                            t * ( 1.00002368 +
-                                            t * ( 0.37409196 + 
-                                            t * ( 0.09678418 + 
-                                            t * (-0.18628806 + 
-                                            t * ( 0.27886807 + 
-                                            t * (-1.13520398 + 
-                                            t * ( 1.48851587 + 
-                                            t * (-0.82215223 + 
-                                            t * ( 0.17087277))))))))));
-        if (z >= 0) return  ans;
-        else        return -ans;
+        System.out.println(poisson(k, lambda));
+    }
+    
+    private static double poisson(int k, double lambda) {
+        return (Math.pow(lambda, k) * Math.pow(Math.E, -1 * lambda)) / factorial(k);
+    }
+    
+    private static Long factorial (int n) {
+        if (n < 0) {
+            return null;
+        }
+        long result = 1;
+        while (n > 0) {
+            result *= n--;
+        }
+        return result;
     }
 }
