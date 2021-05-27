85c4940696f149e68ad79472c56bc7d1cdcef953
Krishna Pala <palakrishnasai@gmail.com>
2021-05-27 12:21:31 +0530

    Update Weather Observation Station 20.sql

--- SQL/Aggregation/Weather Observation Station 20.sql
+++ SQL/Aggregation/Weather Observation Station 20.sql
@@ -1,7 +1,2 @@
-SELECT round(A.LAT_N,4) as median_LAT_N
-FROM (
-SELECT S.LAT_N, @rownum:=@rownum+1 as row_number, @total_rows:=@rownum
-  FROM STATION S, (SELECT @rownum:=0) r
-  ORDER BY S.LAT_N
-) as A
-WHERE A.row_number IN ( FLOOR((@total_rows+1)/2), FLOOR((@total_rows+2)/2) );
+SELECT ROUND(MEDIAN(LAT_N), 4)
+FROM STATION;
